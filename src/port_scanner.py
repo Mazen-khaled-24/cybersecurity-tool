f2c28c745fca1dff0dc676a0a4990c125f010500
Mazen Khaled Mohamed <mazen.khaled2409@gmail.com>
2025-12-06 19:21:17 +0200

    Feature 1: Basic port scanner implemented

---
+++ src/port_scanner.py
@@ -0,0 +1,26 @@
+import socket
+
+def scan_port(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)  # prevent long wait
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            print(f"[OPEN] Port {port}")
+        sock.close()
+    except:
+        pass
+
+def main():
+    print("=== Basic Port Scanner ===")
+    target_ip = input("Enter target IP: ")
+
+    print(f"Scanning {target_ip}...\n")
+
+    for port in range(20, 101):  # small range for now
+        scan_port(target_ip, port)
+
+    print("\nScan complete.")
+
+if __name__ == "__main__":
+    main()
